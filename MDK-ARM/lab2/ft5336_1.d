48af0c6a485b2aa7197381068fab1a1b84e51a3a
Joe <s095339@gmail.com>
2025-06-16 16:26:50 +0800

    first

---
+++ MDK-ARM/lab2/ft5336_1.d
@@ -0,0 +1,4 @@
+lab2\ft5336_1.o: ..\..\..\..\..\STM32Cube\Repository\STM32Cube_FW_F7_V1.15.0\Drivers\BSP\Components\ft5336\ft5336.c
+lab2\ft5336_1.o: ..\..\..\..\..\STM32Cube\Repository\STM32Cube_FW_F7_V1.15.0\Drivers\BSP\Components\ft5336\ft5336.h
+lab2\ft5336_1.o: ..\..\..\..\..\STM32Cube\Repository\STM32Cube_FW_F7_V1.15.0\Drivers\BSP\Components\ft5336\../Common/ts.h
+lab2\ft5336_1.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
---
+++ MDK-ARM/lab2/startup_stm32f746xx.d
@@ -0,0 +1,1 @@
+lab2\startup_stm32f746xx.o: startup_stm32f746xx.s
